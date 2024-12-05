0c7b65c2f8b48deef7872c54d452911223a1d060
SreyashniWepuri <156876591+SreyashniWepuri@users.noreply.github.com>
2024-12-04 21:33:07 -0600

    updating boooking route

--- backend/routes/bookingRoute.js
+++ backend/routes/bookingRoute.js
@@ -2,10 +2,10 @@ const express = require("express");
 const router = express.Router();
 
 const protect = require("../middlewares/auth");
-const { createBooking } = require("../controllers/bookingController");
+const { createBooking, getClientBookings } = require("../controllers/bookingController");
 
 // Route to fetch all users.
 router.post("/bookings", protect, createBooking);
-
+router.get("/client/bookings", protect, getClientBookings);
 
 module.exports = router;

8ed618c102366cadea3892f07f707686a35d2ddc
phanindraraogajula <pgajula@slu.edu>
2024-12-06 23:25:58 -0600

    docker build complete

--- backend/config/config.js
+++ backend/config/config.js
@@ -1,19 +1,24 @@
 const mongoose = require("mongoose");
-
 const connectDB = async () => {
-  // Use environment variable or default to 'mongodb://mongo:27017/professionalmarketdb'
-  const dbURI =
-    process.env.MONGO_URI || "mongodb://mongo:27017/professionalmarketdb";
-
-  mongoose
-    .connect(dbURI, { useNewUrlParser: true, useUnifiedTopology: true })
-    .then(() => {
-      console.log("Connected to MongoDB");
-    })
-    .catch((err) => {
-      console.error("MongoDB connection error:", err);
-      process.exit(1);
+  const dbURI = process.env.MONGO_URI;
+  console.log("Attempting to connect to MongoDB with URI:", dbURI);
+  
+  try {
+    await mongoose.connect(dbURI, { 
+      useNewUrlParser: true, 
+      useUnifiedTopology: true,
+      serverSelectionTimeoutMS: 10000, // 10 seconds
+      socketTimeoutMS: 45000, // 45 seconds
     });
+    console.log("🟢 MongoDB Connection Successful");
+    console.log("Connection Details:", mongoose.connection.host);
+  } catch (err) {
+    console.error("🔴 MongoDB Connection Failed:", err);
+    console.error("Error Name:", err.name);
+    console.error("Error Message:", err.message);
+    console.error("Full Error:", JSON.stringify(err, null, 2));
+    process.exit(1);
+  }
 };
 
 module.exports = connectDB;
